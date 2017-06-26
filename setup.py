f6088ceaf6cc53897363a3661b3b03405153ff94
Anthony Abeo <anthonyabeo@gmail.com>
2017-06-26 12:37:25 +0000

    initial project structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Meet N' Eat",
+    version="1.0",
+    description="REST API backend for a social application for meeting people based on their food interests",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask>=0.12.2',
+        "Flask-Restful>=0.3.6"
+    ]
+)
+
