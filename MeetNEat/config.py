b31c19cad43b4a282669d5e703f96e64e8115111
Anthony Abeo <anthonyabeo@gmail.com>
2017-08-06 15:49:29 +0000

    config cleanup

--- MeetNEat/config.py
+++ MeetNEat/config.py
@@ -5,19 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = 'n#avz%e=lig@=*lxpk=3@$p)iol)2ge8^#w8h1-iks*yb9k@4j'
     CSRF_ENABLED = True
-    OAUTH_CREDENTIALS = {
-
-        'facebook': {
-            'id': '111900076114805',
-            'secret': '8e0cbe1dc9cbe489cd377d76e6bb10c3'
-        },
-
-        'twitter': {
-            'id': 'woPymnfG1CdZq6eKIBoWUVRkB',
-            'secret': 'gxHy3FsPN1jLYmDvO7Gm9Z99PSOwK5GXg9xf2in8NcRSbKhRNx'
-        }
-
-    }
 
     @staticmethod
     def init_app(app):
@@ -29,6 +16,7 @@ class DevelopmentConfig(Config):
 
 
 class StagingConfig(Config):
+    DEBUG = os.environ.get('DEBUG') or True
     TESTING = True
     MONGODB_DB = 'meetneattest'
     MONGODB_HOST = 'localhost'
