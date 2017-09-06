3d47fd960c5f577e3379c977e4b17462e25964a0
Anthony Abeo <anthonyabeo@gmail.com>
2017-09-06 10:31:34 +0000

    LICENSE description

---
+++ meetneat/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_mongoengine import MongoEngine
+
+db = MongoEngine()
+login_manager = LoginManager()
+
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'admin.sign_in'
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('meetneat.config')
+    # config[config_name].init_app(app)
+    # import pdb; pdb.set_trace()
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from api import api_blueprint
+    from admin import admin_blueprint
+
+    app.register_blueprint(api_blueprint)
+    app.register_blueprint(admin_blueprint)
+
+    app.app_context().push()
+
+    return app
